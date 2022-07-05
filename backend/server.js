5ae5550855000bc1bd7c484cfb29c7f44887ad59
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-07-05 13:49:12 -0700

    express app setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+
+
+// express app
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+
+// routes
+app.get('/', (req, res) => {
+  res.json({mssg: 'Welcome to the app'})
+})
+
+// listen for request
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT)
+})
