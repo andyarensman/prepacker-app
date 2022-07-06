864463d220cfc7d188e804d5838a3c0e9486282f
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-07-05 18:38:41 -0700

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const closetRoutes = require('./routes/closet')
 
 
@@ -18,7 +19,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/closet', closetRoutes)
 
-// listen for request
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for request
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
