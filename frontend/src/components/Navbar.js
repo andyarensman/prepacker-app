8ad0c78b56fe8a13c4cb2d06a0d55f8ff9d0b9d3
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-07-09 15:20:42 -0700

    title update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Prepacker</h1>
+          <h1>PrePacker</h1>
         </Link>
       </div>
     </header>
