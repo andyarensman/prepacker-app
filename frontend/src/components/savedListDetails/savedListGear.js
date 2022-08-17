e01618a4a374596ecaae0da1d911b35671f4e11e
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-08-17 14:26:38 -0700

    basic checkbox functionality

--- frontend/src/components/savedListDetails/savedListGear.js
+++ frontend/src/components/savedListDetails/savedListGear.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import { handleWeight } from "../../helpers/utils";
 
 const SavedListGear = ({ gearItem }) => {
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleClick = () => {
+    if (isChecked) {
+      setIsChecked(false)
+    } else {
+      setIsChecked(true)
+    }
+  }
+
   return (
-    <div className="trip-list">
+    <div className="trip-list inline-block">
+      <span className="material-symbols-outlined cancel" onClick={handleClick}>{isChecked ? 'check_box' : 'check_box_outline_blank'}</span>
       {gearItem.gear_name} {gearItem.weight && <i className="weight-italics">({handleWeight(gearItem.weight)}) </i>}
     </div>
   );
