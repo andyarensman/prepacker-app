5434d29091ec40c14e5ef699a977fec46824d145
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-08-19 14:01:11 -0700

    saved lists search bar

--- frontend/src/pages/savedLists.js
+++ frontend/src/pages/savedLists.js
@@ -1,14 +1,44 @@
+import { useEffect, useState } from "react";
 import ChecklistDetails from "../components/savedLists/ChecklistDetails";
 import { useClosetContext } from "../hooks/useClosetContext";
 
 const SavedLists = () => {
+  const [currentSortArr, setCurrentSortArr] = useState([])
+
   const { checklists } = useClosetContext()
+
+  useEffect(() => {
+    if (checklists) {
+      setCurrentSortArr([...checklists])
+    }
+  }, [checklists])
+
+  // Search Functionality
+  const handleChange = (searchWord) => {
+    if (!searchWord) {
+      setCurrentSortArr([...checklists])
+    } else {
+      let regex = new RegExp(searchWord, 'i')
+      let searchResult = checklists.filter(list => regex.test(list.checklist_name))
+      setCurrentSortArr([...searchResult])
+    }
+  }
 
   return ( 
     <div className="saved-lists">
-      <h2>Saved Lists</h2>
+      <div className="saved-lists-header">
+        <h2>Saved Lists</h2>
+        <form onSubmit={(e) => e.preventDefault()} className="search-form">
+          <input 
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => handleChange(e.target.value)}
+          />
+          <button><span className="material-symbols-outlined search-symbol">search</span></button>
+        </form>
+      </div>
       <div className="checklists">
-        {checklists && checklists.map((checklist) => (
+        {checklists && currentSortArr.map((checklist) => (
           <ChecklistDetails key={checklist._id} checklist={checklist} />
         ))}
       </div>
