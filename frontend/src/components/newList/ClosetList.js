8b55e8223d894c2a5095117cb300cb645d6eb8b3
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-08-21 20:06:38 -0700

    closet button change

--- frontend/src/components/newList/ClosetList.js
+++ frontend/src/components/newList/ClosetList.js
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react"
 import { handleWeight } from "../../helpers/utils"
 
 const ClosetList = ({ gear, trip_list, setTripList }) => {
+  const [inList, setInList] = useState(true)
+
+  useEffect(() => {
+    if (trip_list.some(e => e === gear)) {
+      setInList(true)
+      console.log('is in')
+    } else {
+      setInList(false)
+    }
+    
+  }, [trip_list, gear])
+
 
   const handleClick = () => {
-    if (checkTripList() === '+') {
+    if (!inList) {
       let new_trip_list = [...trip_list, gear]
 
       setTripList(new_trip_list)
-    } else if (checkTripList() === '-') {
+    } else if (inList) {
       let index = trip_list.findIndex(x => x._id === gear._id)
 
       let new_trip_list = trip_list.filter((e, i) => i !== index)
@@ -16,17 +29,11 @@ const ClosetList = ({ gear, trip_list, setTripList }) => {
     }
   }
 
-  // Check if gear is in trip_list
-  const checkTripList = () => {
-    if (trip_list.some(e => e === gear)) {
-      return ('-')
-    } else {
-      return ('+')
-    }
-  }
-
   return ( 
-    <div className="closet-list-item"><button onClick={() => handleClick()}>{checkTripList()}</button> {gear.gear_name} {gear.weight && <i className="weight-italics">({handleWeight(gear.weight)})</i>}</div>
+    <div className="closet-list-item">
+      <span onClick={() => handleClick()}className="material-symbols-outlined cancel">{inList ? 'do_not_disturb_on' : 'add_circle'}</span> 
+      {gear.gear_name} {gear.weight && <i className="weight-italics">({handleWeight(gear.weight)})</i>}
+    </div>
    );
 }
  
