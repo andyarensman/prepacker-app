571cf4dd34f4f738e5b92563ee1aff7a819d2d87
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-08-23 10:42:24 -0700

    search component saved lists page

--- frontend/src/pages/savedLists.js
+++ frontend/src/pages/savedLists.js
@@ -1,46 +1,37 @@
 import { useEffect, useState } from "react";
 import ChecklistDetails from "../components/savedLists/ChecklistDetails";
+import TitleSearch from "../components/TitleSearch";
 import { useClosetContext } from "../hooks/useClosetContext";
 
 // css module
 import SavedListsCSS from '../styles/savedLists.module.css'
-import SearchCSS from '../styles/search.module.css'
 
 const SavedLists = () => {
   const [currentSortArr, setCurrentSortArr] = useState([])
+  const [alpha, setAlpha] = useState([])
 
   const { checklists } = useClosetContext()
 
   useEffect(() => {
     if (checklists) {
-      setCurrentSortArr([...checklists])
+      const alphaAsc = [...checklists].sort((a, b) => a.checklist_name.localeCompare(b.checklist_name))
+      setAlpha([...alphaAsc])
+      setCurrentSortArr([...alphaAsc])
     }
   }, [checklists])
 
-  // Search Functionality
-  const handleChange = (searchWord) => {
-    if (!searchWord) {
-      setCurrentSortArr([...checklists])
-    } else {
-      let regex = new RegExp(searchWord, 'i')
-      let searchResult = checklists.filter(list => regex.test(list.checklist_name))
-      setCurrentSortArr([...searchResult])
-    }
-  }
-
   return ( 
     <div className="saved-lists">
-      <div className={SavedListsCSS.savedListsHeader}>
-        <h2 className="search-header">Saved Lists</h2>
-        <form onSubmit={(e) => e.preventDefault()} className={SearchCSS.searchForm}>
-          <input 
-            type="text"
-            placeholder="Search..."
-            onChange={(e) => handleChange(e.target.value)}
-          />
-          <button><span className={`material-symbols-outlined ${SearchCSS.searchSymbol}`}>search</span></button>
-        </form>
-      </div>
+      <TitleSearch 
+        title="Saved Lists"
+        selectNeeded={false}
+        containerClass={SavedListsCSS.savedListsHeader}
+        searchArr={[...alpha]}
+        searchKey="checklist_name"
+        sort={false}
+        setSort={false}
+        setCurrentSortArr={setCurrentSortArr}
+      />
       <div className="checklists">
         {checklists && currentSortArr.map((checklist) => (
           <ChecklistDetails key={checklist._id} checklist={checklist} />
