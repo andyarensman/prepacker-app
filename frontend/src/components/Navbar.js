8a31d370354954f98ac405c8ef7fd3a0fb1373d6
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-08-23 11:54:22 -0700

    css header nav centered and adjusted

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,21 +9,25 @@ const Navbar = () => {
           {/* <h1>PrePacker</h1> */}
           <img src="/PrePacker-logo-1.png" alt="PrePacker" className={NavbarCSS.titleLogo}></img>
         </Link>
-        <Link to="/new-list">
-          <h3>New List</h3>
-        </Link>
-        <Link to="/saved-lists">
-          <h3>Saved Lists</h3>
-        </Link>
-        <Link to="/gear-closet">
-          <h3>My Gear Closet</h3>
-        </Link>
         <nav>
-          <div>
-            <Link to="/">Login</Link>
-            <Link to="/">Signup</Link>
-          </div>
+          
+          <Link to="/new-list">
+            <h3>New List</h3>
+          </Link>
+          <h3>•</h3>
+          <Link to="/saved-lists">
+            <h3>Saved Lists</h3>
+          </Link>
+          <h3>•</h3>
+          <Link to="/gear-closet">
+            <h3>Gear Closet</h3>
+          </Link>
+          
         </nav>
+        <div className={NavbarCSS.login}>
+          <Link to="/">Login</Link>
+          <Link to="/">Signup</Link>
+        </div>
       </div>
       <div className={NavbarCSS.imgContainer}>
         <img src="/Mountains.png" alt="mtn-header" className={NavbarCSS.mountains}></img>
