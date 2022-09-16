15b3654e0c8a83f3ae0dfe613a3424864d8a2de2
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-09-16 14:05:20 -0700

    logging users in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,19 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({mssg: 'login user'})
+  const {email, password} = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+  
 }
 
 // signup user
@@ -19,7 +31,6 @@ const signupUser = async (req, res) => {
 
     // create a token
     const token = createToken(user._id)
-    console.log(token)
 
     res.status(200).json({email, token})
   } catch (error) {
