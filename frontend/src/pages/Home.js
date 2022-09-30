baeb92fb106b1c7a8cbe65d2d172b2822365317b
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-09-30 11:17:28 -0700

    homepage images

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,9 @@
+// images
+import hikerGreenSnowy from '../images/hikers-green-snowy.jpg'
+import gearAll from '../images/gear-all.jpg'
+import tentSunset from '../images/tent-sunset.jpg'
+import mountaineer from '../images/mountaineer.jpg'
+
 const Home = () => {
   return ( 
     <div>
@@ -6,25 +12,25 @@ const Home = () => {
         <p>Helping hikers and backpackers stay organized</p>
         <p>Don't forget that important item!</p>
         <button>Signup</button>
-        {/* Add Image here */}
+        <img src={hikerGreenSnowy} alt="" className=""/>
       </div>
       <div>
         {/* Features */}
         <div>
           <h3>Create Checklists</h3>
           <p>Plan out exactly what you'll need to bring on your next adventure</p>
-          {/* Add Image here */}
+          <img src={mountaineer} alt="" className=""/>
         </div>
         <div>
           <h3>Upload Your Gear</h3>
           <p>Store a digital record of your gear so you can quickly plan your next trip</p>
-          {/* Add Image here */}
+          <img src={gearAll} alt="" className=""/>
         </div>
       </div>
       <div>
         <h3>What are you waiting for?</h3>
-        <button>Start PrePacking</button>
-        {/* Add Image here */}
+        <button>Start PrePacking!</button>
+        <img src={tentSunset} alt="" className=""/>
       </div>
     </div>
    );
