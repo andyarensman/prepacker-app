27342857ff81d682746b0177d353d9e9ae9f7482
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-09-29 19:38:04 -0700

    basic home page html

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,32 @@
 const Home = () => {
   return ( 
-    <h2>Home Page TBD</h2>
+    <div>
+      <div>
+        <h2>Pack Your Next Trip</h2>
+        <p>Helping hikers and backpackers stay organized</p>
+        <p>Don't forget that important item!</p>
+        <button>Signup</button>
+        {/* Add Image here */}
+      </div>
+      <div>
+        {/* Features */}
+        <div>
+          <h3>Create Checklists</h3>
+          <p>Plan out exactly what you'll need to bring on your next adventure</p>
+          {/* Add Image here */}
+        </div>
+        <div>
+          <h3>Upload Your Gear</h3>
+          <p>Store a digital record of your gear so you can quickly plan your next trip</p>
+          {/* Add Image here */}
+        </div>
+      </div>
+      <div>
+        <h3>What are you waiting for?</h3>
+        <button>Start PrePacking</button>
+        {/* Add Image here */}
+      </div>
+    </div>
    );
 }
  
