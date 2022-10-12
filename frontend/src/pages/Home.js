a01cf919b07f73f771c9822207cc1d69bef8b077
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-10-12 12:50:34 -0700

    homepage css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,13 +9,13 @@ import HomeCSS from '../styles/Home.module.css'
 
 const Home = () => {
   return ( 
-    <div>
+    <div className={HomeCSS.page}>
       <div className={HomeCSS.gridContainer}>
-        <div>
-          <h2>Pack Your Next Trip</h2>
-          <p>Helping hikers and backpackers stay organized</p>
-          <p>Don't forget that important item!</p>
-          <button>Signup</button>
+        <div className={HomeCSS.textContainer}>
+          <h1>Pack Your Next Trip</h1>
+          <p><i>- Helping hikers and backpackers stay organized -</i></p>
+          <p>Never forget that important item!</p>
+          <button className={HomeCSS.button}>Signup</button>
         </div>
         <img src={hikerGreenSnowy} alt="" className=""/>
       </div>
@@ -23,22 +23,32 @@ const Home = () => {
       {/* Features */}
       <div className={HomeCSS.gridContainer}>
         <img src={mountaineer} alt="" className=""/>
-        <div>
-          <h3>Create Checklists</h3>
-          <p>Plan out exactly what you'll need to bring on your next adventure</p>
+        <div className={HomeCSS.textContainer}>
+          <h2>Create Checklists</h2>
+          <ul className={HomeCSS.noBottom}>
+            <li>Plan out exactly what you'll need to bring on your next adventure</li>
+            <li>Automatically see how much your pack could weigh</li>
+            <li>Easily adjust your list to make the perfect trip</li>
+            <li>Save and edit lists to remember what did and didn't work</li>
+          </ul>
         </div>
       </div>
       <div className={HomeCSS.gridContainer}>
-        <div>
-          <h3>Upload Your Gear</h3>
-          <p>Store a digital record of your gear so you can quickly plan your next trip</p>
+        <div className={HomeCSS.textContainer}>
+          <h2>Upload Your Gear</h2>
+          <ul className={HomeCSS.noBottom}>
+            <li>Store a digital record of each piece of gear so you can quickly plan your next trip</li>
+            <li>Track weight, category, price, and more</li>
+            <li>Upload data directly from other websites to save time</li>
+          </ul>
         </div>
         <img src={gearAll} alt="" className=""/>
       </div>
       
-      <div>
-        <h3>What are you waiting for?</h3>
-        <button>Start PrePacking!</button>
+      <div className={HomeCSS.finalCall}>
+        <h2>What are you waiting for?</h2>
+        <button className={HomeCSS.button}>Start PrePacking!</button>
+        <br />
         <img src={tentSunset} alt="" className=""/>
       </div>
     </div>
