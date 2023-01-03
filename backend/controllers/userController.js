3b9da99396bc81bab8ef7ea152f2cc95ace497d0
andyarensman <86622025+andyarensman@users.noreply.github.com>
2023-01-03 10:56:59 -0800

    auth test

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,8 +2,8 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
-  // return jwt.sign({_id}, process.env.SECRET, { expiresIn: '10s' })
+  // return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '10s' })
 }
 
 // login user
